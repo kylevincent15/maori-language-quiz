5b7a627d76e752c21e4bdec11e142cdd893d559a
Kyle Vincent <105256710+kylevincent15@users.noreply.github.com>
2022-05-17 01:42:17 +0000

    added the questions and how they cycle

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ class Question:
      def __init__(self, prompt, answer):
           self.prompt = prompt
           self.answer = answer
-
+       
 question_prompts = [
-     "What word means two in maori?\n(a) Rua\n(b)Ono",
-     "What color are bananas in moari?\n(a) whero/kākāriki \n(b)kōwhai ",
+     "What word means two in maori?\n(a) Rua\n(b) Ono ",
+     "What color are bananas in moari?\n(a) whero\n(b)kōwhai ",
+     "What color are apples in moari?\n(a) whero\n(b)kahurangi ",
+     "how do you say hi how are you in moari?\n(a) hi kei te pehea koe\n(b) he pop koe ", 
+     "what is cat in maori?\n(a) ngeru \n(b) kuri "
 ]
 
 questions = [
      Question(question_prompts[0], "a"),
      Question(question_prompts[1], "b"),
+     Question(question_prompts[2], "a"), 
+     Question(question_prompts[3], "a"),
+     Question(question_prompts[4], "b"),
 ]
 
 def run_quiz(questions):
@@ -21,4 +27,4 @@ def run_quiz(questions):
                score += 1
      print("you got", score, "out of", len(questions))
 
-run_quiz(questions)
+run_quiz(questions) 
